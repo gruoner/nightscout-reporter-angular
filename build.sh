ae973a1535e5f071378e84337dca6435fcf10123
gruoner <Weich1ei!>
2024-06-23 16:47:07 +0200

    build Skripte

---
+++ build.sh
@@ -0,0 +1,6 @@
+export https_proxy="http://10.230.1.1:8080"
+export http_proxy="http://10.230.1.1:8080"
+export NODE_EXTRA_CA_CERTS="/usr/share/ca-certificates/extra/WebProxy-CA.crt"
+
+npm run build --base-href /NightScoutReporter-angular/
+
---
+++ prepare.sh
@@ -0,0 +1,7 @@
+export https_proxy="http://10.230.1.1:8080"
+export http_proxy="http://10.230.1.1:8080"
+export NODE_EXTRA_CA_CERTS="/usr/share/ca-certificates/extra/WebProxy-CA.crt"
+
+npm install -g @angular/cli
+npm install --save-dev @angular-devkit/build-angular
+
