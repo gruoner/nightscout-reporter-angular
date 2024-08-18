817bf91440c7519a6552e3a18cac2720ae4a565c
gruoner <gruoner@github>
2024-08-18 17:20:38 +0200

    nun baut es anständig prepare muss als root aufgerufen werden

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@ export https_proxy="http://10.230.1.1:8080"
 export http_proxy="http://10.230.1.1:8080"
 export NODE_EXTRA_CA_CERTS="/usr/share/ca-certificates/extra/WebProxy-CA.crt"
 
-npm run build --base-href /NightScoutReporter-angular/
+#npm run build --baseHref /NightScoutReporter-angular
+ng build --output-path dist/nightrep --base-href /NightScoutReporter-angular/ --deploy-url /NightScoutReporter-angular/
+
+cp -a dist/nightrep/* /var/www/html/NightScoutReporter-angular
 
