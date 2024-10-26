c4c1cd70eb76d1c5c72fb4024f42ac7161eb15bd
gruoner <gruoner@github>
2024-10-26 22:20:46 +0200

    prepare muss in zwei Schritten laufen - installation des angular-cli als root - installation von angular als User

--- prepareOnceAsroot.sh
+++ prepareOnceAsroot.sh
@@ -2,6 +2,5 @@ export https_proxy="http://10.230.1.1:8080"
 export http_proxy="http://10.230.1.1:8080"
 export NODE_EXTRA_CA_CERTS="/usr/share/ca-certificates/extra/WebProxy-CA.crt"
 
-npm install --save-dev @angular-devkit/build-angular
-npm audit fix
+npm install -g @angular/cli
 
